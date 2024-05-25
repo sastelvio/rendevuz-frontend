f6fe38466c7bc20f04a15275c0a6421bb1db3200
sastelvios <sast.serafim@gmail.com>
2024-05-25 22:53:21 +0200

    update: login [UI]

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -9,11 +9,17 @@ import {
     Button,
     Grid,
     Alert,
+    Card,
+    CardContent,
 } from "@mui/material";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useAppDispatch } from "../hooks/redux-hooks";
 import { login } from "../slices/authSlice";
+import LockPersonIcon from '@mui/icons-material/LockPerson';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
 
 const Login = () => {
     const dispatch = useAppDispatch();
@@ -45,7 +51,9 @@ const Login = () => {
     };
 
     return (
-        <>
+        <Box sx={{ display: "flex", width: '100%', height: '100vh', overflow: 'hidden', backgroundColor: '#e5e5e5' }}>
+            <CssBaseline />
+
             <Container maxWidth="xs">
                 <CssBaseline />
                 <Box
@@ -56,55 +64,115 @@ const Login = () => {
                         alignItems: "center",
                     }}
                 >
-                    <Avatar sx={{ m: 1, bgcolor: "primary.light" }}>
-                        <LockOutlined />
-                    </Avatar>
-                    <Typography variant="h5">Login</Typography>
-                    <Box sx={{ mt: 1 }}>
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="username"
-                            label="Username"
-                            name="username"
-                            autoFocus
-                            value={username}
-                            onChange={(e) => setUserName(e.target.value)}
-                        />
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="password"
-                            name="password"
-                            label="Password"
-                            type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                        {error && (
-                            <Alert severity="error" sx={{ mt: 2 }}>
-                                {error}
-                            </Alert>
-                        )}
-                        <Button
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                            onClick={handleLogin}
+                    <Box>
+                        <Card
+                            sx={{
+                                paddingTop: 4,
+                                paddingRight: 3,
+                                paddingBottom: 5,
+                                paddingLeft: 1,
+                                backgroundColor: 'transparent',
+                                boxShadow: 'none',
+                            }}
                         >
-                            Login
-                        </Button>
-                        <Grid container justifyContent={"flex-end"}>
-                            <Grid item>
-                                <Link to="/register">Don't have an account? Register</Link>
-                            </Grid>
-                        </Grid>
+                            <Box
+                                sx={{
+                                    position: 'relative',
+                                    paddingTop: 3,
+                                    backgroundColor: 'white',
+                                    boxShadow: '5px 5px 15px rgba(0, 0, 0, 0.6)',
+                                    borderRadius: 2,
+                                }}
+                            >
+                                <Box
+                                    sx={{
+                                        backgroundColor: '#1976d2',
+                                        color: '#fff',
+                                        padding: 0,
+                                        paddingLeft: 2,
+                                        boxShadow: '5px 5px 15px rgba(0, 0, 0, 0.3)',
+                                        borderRadius: 1,
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
+                                        position: 'absolute',
+                                        top: -30, // Mover para cima para criar efeito de flutuação
+                                        left: '50%',
+                                        transform: 'translateX(-50%)',
+                                        width: '90%',
+                                        height: '100px', // Ajuste a altura conforme necessário
+                                    }}
+                                >
+                                    <Typography variant="h5" sx={{ fontWeight: 'bold' }}>Sign In</Typography>
+                                </Box>
+                                <CardContent sx={{ marginTop: 5 }}> {/* Espaço para acomodar o efeito de flutuação */}
+                                    <Box sx={{ mt: 1 }}>
+                                        <TextField
+                                            margin="normal"
+                                            required
+                                            fullWidth
+                                            id="username"
+                                            label="Username"
+                                            name="username"
+                                            autoFocus
+                                            value={username}
+                                            onChange={(e) => setUserName(e.target.value)}
+                                        />
+                                        <TextField
+                                            margin="normal"
+                                            required
+                                            fullWidth
+                                            id="password"
+                                            name="password"
+                                            label="Password"
+                                            type="password"
+                                            value={password}
+                                            onChange={(e) => setPassword(e.target.value)}
+                                        />
+                                        {error && (
+                                            <Alert severity="error" sx={{ mt: 2 }}>
+                                                {error}
+                                            </Alert>
+                                        )}
+                                        <FormGroup>
+                                            <FormControlLabel control={<Switch />} label="Remember me" />
+                                        </FormGroup>
+                                        <Button
+                                            variant="contained"
+                                            color="success"
+                                            fullWidth
+                                            sx={{
+                                                marginTop: 2,
+                                            }}
+                                            onClick={handleLogin}
+                                            autoFocus
+                                            endIcon={<LockPersonIcon />}
+                                        >
+                                            Authenticate
+                                        </Button>
+                                        <Grid container justifyContent={"flex-center"}>
+                                            <Grid item xs={12}>
+                                                <Button
+                                                    fullWidth
+                                                    color="primary"
+                                                    variant="text"
+                                                    sx={{
+                                                        marginTop: 2,
+                                                        width: '100%'
+                                                    }}
+                                                >
+                                                    I forgot my credentials
+                                                </Button>
+                                            </Grid>
+                                        </Grid>
+                                    </Box>
+                                </CardContent>
+                            </Box>
+                        </Card>
                     </Box>
                 </Box>
             </Container>
-        </>
+        </Box>
     );
 };
 
